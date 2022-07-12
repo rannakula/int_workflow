51b58a6876f96554ceed79da53caef80d2db3ca2
rannakula <91966596+rannakula@users.noreply.github.com>
2022-07-12 17:58:44 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -24,4 +24,12 @@ resource "aws_s3_bucket" "mfadelete3" {
     mfa_delete = true
   }
 }
+resource "aws_s3_bucket" "mfadelete4" {
+  bucket = "examplebuckettftest4"
+  acl    = "private"
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+}
 
